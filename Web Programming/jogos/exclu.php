c94161a1c3a05c578e6279aa0da3599786ef0330
Miguel Chichorro de Oliveira <miguelchichorro1@gmail.com>
2021-05-17 16:50:51 -0300

    Delete Web Programming/jogos directory

--- Web Programming/jogos/exclu.php
+++
@@ -1,9 +0,0 @@
-<?php
-include('conexao.php');
-if (isset($_GET['jog'])) {
- $query = "DELETE FROM tb_jogos WHERE cd_jogos = '". $_GET['jog'] . "'";
- if ($result = $mysqli->query($query)) {
-   header("location:Outro.php");
- }
-}
- ?>

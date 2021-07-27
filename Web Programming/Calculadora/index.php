e306673b146d69aaf249ddf1a307f055cff2f1e7
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-27 15:41:48 -0300

    Code Indentation

--- Web Programming/Calculadora/index.php
+++ Web Programming/Calculadora/index.php
@@ -1,16 +1,16 @@
 <?php session_start();
-if(!isset($_SESSION['resposta'])) {
-  $_SESSION['resposta'] = 0;
-}
- ?>
- <!DOCTYPE html>
- <html>
-   <head>
-     <link rel="stylesheet" href="style.css">
-     <meta charset="utf-8">
-     <title>calcu</title>
-     <?php
-     if(isset($_POST['calcu'])){
+  if(!isset($_SESSION['resposta'])){
+    $_SESSION['resposta'] = 0;
+  }
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <link rel="stylesheet" href="style.css">
+    <meta charset="utf-8">
+    <title>calcu</title>
+    <?php
+      if(isset($_POST['calcu'])){
        if ($_POST['calcu'] == "somar") {
          $_SESSION['resposta'] += $_POST['numero'];
        }
@@ -24,37 +24,30 @@ if(!isset($_SESSION['resposta'])) {
          $_SESSION['resposta'] /= $_POST['numero'];
        }
        $resposta = $_SESSION['resposta'];
-     }
-
-      ?>
-
-   </head>
-   <body>
-     <center>
-       <fieldset id="fil">
-         <legend><fieldset>CALCULADORA 5K</fieldset></legend>
-     <form method="post">
-     <input type="number" value="<?php echo $_SESSION['resposta'];?>" disabled><br><br>
-     <input type="number" name="numero" placeholder="Faça sua conta"><br>
-     <label><input type="radio" name="calcu" value="somar">+Somar+</label><br>
-     <label><input type="radio" name="calcu" value="subtrair">-Subtrair-</label><br>
-     <label><input type="radio" name="calcu" value="multiplicar">*Multiplicar*</label><br>
-     <label><input type="radio" name="calcu" value="dividir">/Dividir/</label><br><br>
-     <input type="submit" value="Calcular">
-
-</form>
-<fieldset>
-
-
-  <legend>RESPOSTA</legend>
-  <?php
-
-    echo $_SESSION['resposta'];
-?>
-</fieldset>
-
-</p>
-</fieldset>
-</center>
-   </body>
- </html>
+      }
+    ?>
+  </head>
+  <body>
+    <center>
+      <fieldset id="fil">
+        <legend><fieldset>CALCULADORA 5K</fieldset></legend>
+        <form method="post">
+          <input type="number" value="<?php echo $_SESSION['resposta'];?>" disabled><br><br>
+          <input type="number" name="numero" placeholder="Faça sua conta"><br>
+          <label><input type="radio" name="calcu" value="somar">+Somar+</label><br>
+          <label><input type="radio" name="calcu" value="subtrair">-Subtrair-</label><br>
+          <label><input type="radio" name="calcu" value="multiplicar">*Multiplicar*</label><br>
+          <label><input type="radio" name="calcu" value="dividir">/Dividir/</label><br><br>
+          <input type="submit" value="Calcular">
+        </form>
+        <fieldset>
+          <legend>RESPOSTA</legend>
+          <?php
+            echo $_SESSION['resposta'];
+          ?>
+        </fieldset>
+        </p>
+      </fieldset>
+    </center>
+  </body>
+</html>

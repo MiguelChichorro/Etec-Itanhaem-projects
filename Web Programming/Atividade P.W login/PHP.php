4c3e298c4807a79d055a35ffcc20f0432d184c43
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-07-27 15:57:35 -0300

    Code Indentation

--- Web Programming/Atividade P.W login/PHP.php
+++ Web Programming/Atividade P.W login/PHP.php
@@ -1,15 +1,15 @@
 <style media="screen">
-body{
-background-color: black;
-color: white;
-}
-fieldset{
-  height: auto;
-  width: 300px;
-  border-radius: 25px;
-  margin-top: 100px;
-}
-button{
+  body{
+    background-color: black;
+    color: white;
+  }
+  fieldset{
+    height: auto;
+    width: 300px;
+    border-radius: 25px;
+    margin-top: 100px;
+  }
+  button{
     height: 35px;
     width: 100px;
     border-radius: 25px;
@@ -25,30 +25,28 @@ button{
   }
 </style>
 <?php
-$login=0;
-echo "<meta charset=utf-8>";
-echo "<center>";
-echo "<fieldset>";
-if ($_POST['email']=="joghl@hotmail.com") {
-   $login++;
-}
-
-if ($_POST['senha']=="123") {
-  $login++;
-}
-
-if ($login==2) {
-  echo "<h1>Parabéns você entrou</h1>";
-  echo "E-mail correto:joghl@hotmail.com";
+  $login=0;
+  echo "<meta charset=utf-8>";
+  echo "<center>";
+  echo "<fieldset>";
+  if ($_POST['email']=="joghl@hotmail.com") {
+    $login++;
+  }
+  if ($_POST['senha']=="123") {
+    $login++;
+  }
+  if ($login==2) {
+    echo "<h1>Parabéns você entrou</h1>";
+    echo "E-mail correto:joghl@hotmail.com";
+    echo "<br>";
+    echo "Senha correta: 123";
+  }
+  else {
+    echo "VISH<br>TENTE NOVAMENT<br>UMA DICA: USE O PLACEHOLDER";
+  }
   echo "<br>";
-  echo "Senha correta: 123";
-}
-else {
-  echo "VISH<br>TENTE NOVAMENT<br>UMA DICA: USE O PLACEHOLDER";
-}
-echo "<br>";
-echo "<br>";
-echo "<button type=button name=button onclick=window.open('index.html'); window.close();>Tentar novamente</button>";
-echo "</fieldset>";
-echo "</center>";
- ?>
+  echo "<br>";
+  echo "<button type=button name=button onclick=window.open('index.html'); window.close();>Tentar novamente</button>";
+  echo "</fieldset>";
+  echo "</center>";
+?>

b1a093305b5ffcd114ebbe226a83fd3080bbe407
Miguel Chichorro de Oliveira <51164882+MiguelChichorro@users.noreply.github.com>
2021-09-11 11:44:50 -0300

    Create javascript document

---
+++ Website development/Countries/Canada/CN.js
@@ -0,0 +1,79 @@
+$(document).ready(function(){
+    $(window).scroll(function() {
+        if ($(this).scrollTop()) {
+            $('#myBtn').fadeIn();
+        } 
+        else {
+            $('#myBtn').fadeOut();
+        }
+    });
+    $("#myBtn").click(function () {
+        $("html, body").animate({scrollTop: 0}, 1000);
+    });
+    $(".inside-box").hide();
+    $(".begin").show();
+    $(".logo").click(function(){
+        $(".inside-box").hide();
+        $(".title").css("text-decoration", "unset");
+        $(".begin").show();
+    });
+    $("#cities").click(function(){
+        $(".inside-box").hide();
+        $(".begin").hide();
+        $("#cities-box").show();
+        $(".title").css("text-decoration", "unset");
+        $("#cities").css("text-decoration", "underline");
+    });
+    $("#cultures").click(function(){
+        $(".inside-box").hide();
+        $(".begin").hide();
+        $("#cultures-box").show();
+        $(".title").css("text-decoration", "unset");
+        $("#cultures").css("text-decoration", "underline");
+    });
+    $("#tourist").click(function(){
+        $(".inside-box").hide();
+        $(".begin").hide();
+        $("#tourist-box").show();
+        $(".title").css("text-decoration", "unset");
+        $("#tourist").css("text-decoration", "underline");
+    });
+    $("#about").click(function(){
+        $(".inside-box").hide();
+        $(".begin").hide();
+        $("#about-box").show();
+        $(".title").css("text-decoration", "unset");
+        $("#about").css("text-decoration", "underline");
+    });
+});
+$(window).on('load', function () {
+    $('#loading').hide();
+  }) 
+var slideIndex = 1;
+showSlides(slideIndex);
+        
+// Next/previous controls
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+        
+// Thumbnail image controls
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+        
+function showSlides(n) {
+    var i;
+    var slides = document.getElementsByClassName("mySlides");
+    var dots = document.getElementsByClassName("dot");
+    if (n > slides.length) {slideIndex = 1}
+    if (n < 1) {slideIndex = slides.length}
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    slides[slideIndex-1].style.display = "block";
+    dots[slideIndex-1].className += " active";
+}
